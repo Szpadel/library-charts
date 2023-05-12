e177d52bd2a0bb76b4bd5cb41a648ba15fefd579
Piotr Rogowski <piotr.rogowski@creativestyle.pl>
2023-05-12 12:06:38 +0200

    Migrate unit tests

--- scripts/test-helm-unittest.sh
+++ scripts/test-helm-unittest.sh
@@ -5,9 +5,9 @@ DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")../"
 if ! [ -e ~/.local/share/helm/plugins/helm-unittest ];then
     # it might be broken symlint therefore we try to remove it anyways
     rm -f ~/.local/share/helm/plugins/helm-unittest
-    helm plugin install https://github.com/quintush/helm-unittest
+    helm plugin install https://github.com/helm-unittest/helm-unittest
 fi
 
 helm unittest \
-    -3 ./testing-charts/common-test \
+    ./testing-charts/common-test \
     -f "tests/**/*_test.yaml"
